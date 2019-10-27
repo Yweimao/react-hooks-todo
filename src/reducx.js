06993d976cf3f8fe411c701fadb5c79b27c9b2a9
yeweimao <1097564183@qq.com>
2019-10-27 23:50:35 +0800

    todo

--- src/reducx.js
+++ src/reducx.js
@@ -1,33 +1,60 @@
-import React, { useState, useEffect,useMemo, useRef,useCallback } from 'react';
+import React, {useState, useEffect, useMemo, useRef, useCallback} from 'react';
 import './App.css'
-function Control() {
+
+let idSeq = Date.now()
+function Control(props) {
+    const {addTodo} = props;
+    const inputRef = useRef()
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const newText = inputRef.current.value.trim();
+        if (newText.length === 0) {
+            return
+        }
+        addTodo({
+            id: idSeq++,
+            text: newText,
+            complate: false
+        })
+        inputRef.current.value = ''
+    }
     return (
-        <div></div>
+        <div className='control'>
+            <h1>todos</h1>
+            <form action="" onSubmit={onSubmit}>
+                <input type="text"
+                       ref={inputRef}
+                       className='new-todo'
+                       placeholder='what needs to be done?'/>
+            </form>
+        </div>
     )
 }
 
 
-function Todos() {
-    return(
+function Todos(props) {
+    console.log(props)
+    const {removeTodo, toggleTodo, todos } = props;
+    return (
         <div>
         </div>
     )
 }
 
 function TodoList() {
-    const [todos, setTodos] =useState([])
+    const [todos, setTodos] = useState([])
     const addTodo = useCallback((todo) => {
-        setTodos( todos => [...todos, todo])
-    },[])
-    const removeTodo = useCallback( (id) => {
-        setTodos( todos => todos.filter( todo => {
+        setTodos(todos => [...todos, todo])
+    }, [])
+    const removeTodo = useCallback((id) => {
+        setTodos(todos => todos.filter(todo => {
             return todo.id !== id
         }))
     }, [])
 
     const toggleTodo = useCallback((id) => {
-        setTodos( todos => todos.map( todo => {
-            return todo.id  === id
+        setTodos(todos => todos.map(todo => {
+            return todo.id === id
                 ? {
                     ...todo,
                     complate: !todo.complate
@@ -35,10 +62,10 @@ function TodoList() {
                 : todo
         }))
     }, [])
-    return(
+    return (
         <div className='todo-list'>
             <Control addTodo={addTodo}></Control>
-            <Todos removeTo={removeTodo} toggleTodo={toggleTodo}></Todos>
+            <Todos removeTo={removeTodo} toggleTodo={toggleTodo} todos={todos}></Todos>
         </div>
     )
 }
